b24d39b1aea98e30a10147bcb660c7acc86f8b1c
PepegaSlayer <129308099+PepegaSlayer@users.noreply.github.com>
2024-05-08 01:56:56 +0500

    Working calendar

--- src/app/components/calendar/calendar component/ui.calendar.component.ts
+++ src/app/components/calendar/calendar component/ui.calendar.component.ts
@@ -9,6 +9,25 @@ import { CommonModule } from '@angular/common';
   styleUrl: './ui.calendar.component.scss'
 })
 export class UICalendarComponent {
-  days: number[] =  Array.from({length: 30}, (_, i) => i + 1);
+
+  todayDate = new Date();
   names: string[] = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+  months: string[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  currentMonth = this.todayDate.getMonth();
+  currentYear: number = this.todayDate.getFullYear();
+  days: number[] = Array.from({length: new Date(this.currentYear, this.currentMonth + 1, 0).getDate()}, (_, i) => i + 1);
+  
+  public changeMonth(diff: number):void {
+      this.currentMonth += diff
+      if (this.currentMonth < 0) {
+        this.currentMonth = 11;
+        this.currentYear -= 1;
+      } 
+      else if (this.currentMonth > 11) {
+        this.currentMonth = 0;
+        this.currentYear += 1;
+      } 
+
+      this.days = Array.from({length: new Date(this.currentYear, this.currentMonth + 1, 0).getDate()}, (_, i) => i + 1);
+  }
 }
