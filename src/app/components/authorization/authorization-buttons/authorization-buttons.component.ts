4ef38dfa53cbda0dac92f345eda47ead25d35889
PepegaSlayer <129308099+PepegaSlayer@users.noreply.github.com>
2024-06-02 01:39:53 +0500

    Add Output to login modal

--- src/app/components/authorization/authorization-buttons/authorization-buttons.component.ts
+++ src/app/components/authorization/authorization-buttons/authorization-buttons.component.ts
@@ -1,17 +1,12 @@
 import {
     Component,
-    ComponentRef,
-    inject,
-    ViewChild,
-    ViewContainerRef,
+    EventEmitter,
+    Output,
 } from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {NgIf, NgTemplateOutlet} from '@angular/common';
 import {RegisterComponent} from '../register/register.component';
 import {LoginComponent} from '../login/login.component';
-import {Injector} from '@angular/core';
-import {ModalService} from '../../../services/modal-services/modal.service';
-
 @Component({
     selector: 'app-authorization-buttons',
     standalone: true,
@@ -29,30 +24,8 @@ export class AuthorizationButtonsComponent {
     protected registerButton: string = 'sign up';
     protected logButton: string = 'log in';
 
-    public injector: Injector = inject(Injector);
+    @Output() loginEvent = new EventEmitter<void>();
+    @Output() signupEvent = new EventEmitter<void>();
 
-    @ViewChild('register', {read: ViewContainerRef, static: true})
-    public registerTemplate!: ViewContainerRef;
 
-    @ViewChild('login', {read: ViewContainerRef, static: true})
-    public loginTemplate!: ViewContainerRef;
-
-    constructor(private modalService: ModalService) {
-    }
-
-    public openRegisterModal(): void {
-        const registerComponent: ComponentRef<RegisterComponent> =
-            this.registerTemplate.createComponent(RegisterComponent, {
-                injector: this.injector,
-            });
-        this.modalService.setComponentRef(registerComponent);
-    }
-
-    public openLoginModal(): void {
-        const loginComponent: ComponentRef<LoginComponent> =
-            this.loginTemplate.createComponent(LoginComponent, {
-                injector: this.injector,
-            });
-        this.modalService.setComponentRef(loginComponent);
-    }
 }
